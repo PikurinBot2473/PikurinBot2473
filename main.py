d1ea044e7bb684ff82a0d963561740894f30b971
PikurinBot2473 <pikurinbot2473@gmail.com>
2025-12-30 13:52:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,98 +1,74 @@
-runproject = 1
-if runproject == 0:
-    exit()
-
 from keep_alive import keep_alive
 import discord
-from discord.opus import Encoder as OpusEncoder
-import shlex
-import subprocess
-import logging
 import asyncio
-import datetime
-import os
-import sys
+import logging
 
-audio_url = "https://www.dropbox.com/scl/fi/2ddc7tthdmel26nk97wgs/1-study-timer-60.mp3?rlkey=rlz51afwy8ls6ucxesdr7ulku&st=p5sigche&dl=1"
+audio_url = "https://www.dropbox.com/scl/fi/2ddc7tthdmel26nk97wgs/1-study-timer-60.mp3?dl=1"
 
-#DiscordBotのトークン
 TOKEN = 'MTI3NzE4Nzc3NDg5MzUyMjk3OA.Ge22eo.YlUlQjm5eqB0kAMKmoLSNiVvRAraA-t2Xw46F0'
-#チャンネルID
 channel_id = 1277188290121961586
 
-
-
-intents=discord.Intents.all()
+intents = discord.Intents.all()
 intents.typing = False
 client = discord.Client(intents=intents)
+
+logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
+
+
+async def ensure_voice(channel):
+    vc = channel.guild.voice_client
+
+    if vc is None:
+        await channel.connect(self_deaf=True)
+        return
+
+    if not vc.is_connected():
+        await vc.disconnect(force=True)
+        await channel.connect(self_deaf=True)
+
+
+async def play_loop(channel):
+    while True:
+        try:
+            await ensure_voice(channel)
+            vc = channel.guild.voice_client
+
+            # VCがまだ無い・切断中なら待つ
+            if vc is None or not vc.is_connected():
+                await asyncio.sleep(2)
+                continue
+
+            # 再生中なら待機
+            if vc.is_playing():
+                await asyncio.sleep(5)
+                continue
+
+            source = discord.FFmpegPCMAudio(
+                audio_url,
+                before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+            )
+
+            vc.play(source)
+
+            # 再生終了まで待つ
+            while vc.is_playing():
+                await asyncio.sleep(1)
+
+        except Exception as e:
+            log.exception("play_loop error")
+            await asyncio.sleep(5)  # 落ち着かせて再試行
 
 
 @client.event
 async def on_ready():
     print('ログインしました')
-    await client.change_presence(activity=discord.Game(name='Pikurinサーバー専用BOT'))
-    logch = client.get_channel(1133638395030163486)
-    botlog = await logch.send('ボットが起動しました。')
+    await client.change_presence(
+        activity=discord.Game(name='Pikurinサーバー専用BOT')
+    )
 
     channel = client.get_channel(channel_id)
-    guild = channel.guild
-
-
-    await asyncio.sleep(2)
-    await play_music_in_voice_channels(channel, guild, botlog)
-
-
-
-async def play_music(channel, guild, botlog):
-    try:
-        if guild.voice_client:
-            await guild.voice_client.disconnect()
-            await asyncio.sleep(2)
-        await channel.connect()
-        await guild.change_voice_state(channel=guild.voice_client.channel, self_deaf=True)
-        print('VCに参加成功')
-        guild.voice_client.play(discord.FFmpegPCMAudio(audio_url, before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"))
-        print('play!')
-    except Exception as e:
-        print(e)
-        try:
-            await botlog.edit(content=f'おっと！何かエラーが発生したようです。\nエラーのタイミング：ボイスチャンネルに接続して音楽を再生するとき\n```\n{e}\n```\n')
-        except:
-            await botlog.edit(content=f'おっと！何かエラーが発生したようです。\nエラーのタイミング：ボイスチャンネルに接続して音楽を再生するとき\nエラーログは長すぎて送信できません。')
-        await botlog.channel.send('再起動します。')
-        os.execv(sys.executable, ['python'] + sys.argv)
-    
-    while True:
-        try:
-            if guild.voice_client:
-                while guild.voice_client.is_playing():
-                    await asyncio.sleep(5)
-                guild.voice_client.play(discord.FFmpegPCMAudio(audio_url, before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"))
-        except Exception as e:
-            print(e)
-            try:
-                await botlog.edit(content=f'おっと！何かエラーが発生したようです。\nエラーのタイミング：2回目以降で音楽を再生するとき\n```\n{e}\n```')
-            except:
-                await botlog.edit(content=f'おっと！何かエラーが発生したようです。\nエラーのタイミング：2回目以降で音楽を再生するとき\nエラーログは長すぎて送信できません。')
-            await botlog.channel.send('再起動します。')
-            os.execv(sys.executable, ['python'] + sys.argv)
-
-
-
-
-
-
-
-async def play_music_in_voice_channels(channel, guild, botlog):
-    client.loop.create_task(play_music(channel, guild, botlog))
-
-
-
-
-
-
-
+    client.loop.create_task(play_loop(channel))
 
 
 keep_alive()
